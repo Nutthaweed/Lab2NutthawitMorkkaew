4b996f5c431d3e7117994a8445aa34c09d1d82bf
Nutthaweed <toto24.2546@gmail.com>
2024-12-23 13:08:24 +0700

    I love tongpool

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine' , 'ejs')
+
+let todos = [];
+
+app.get('/', (req,res) => {
+    res.render('index', {todos})    
+})
+
+app.listen(PORT , () => {
+    console.log(`listening on http://localhost:${PORT}`)
+})
